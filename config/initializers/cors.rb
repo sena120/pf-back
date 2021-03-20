30f014b641cec6a8fde7fbf923e1a3b13a929a3e
hanada <hsena120@gmail.com>
2021-03-20 15:59:48 +0900

    corsの変更

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,7 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000',
-    'https://foodlist-git-main-sena120.vercel.app'
+    'https://foodlist.vercel.app'
 
 
     resource '*',
